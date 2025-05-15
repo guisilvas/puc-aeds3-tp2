601ec4ee324c739502e305131bfbaeafc082cf01
guisilva <guilhermesoaressilvadev@gmail.com>
2025-05-15 16:42:25 -0300

    Update model ActorFile.java

--- src/pucflix/model/ActorFile.java
+++ src/pucflix/model/ActorFile.java
@@ -1,92 +1,124 @@
 package pucflix.model;
 
-import pucflix.entity.Episode;
 import pucflix.aeds3.Arquivo;
 import pucflix.aeds3.HashExtensivel;
 import pucflix.aeds3.ArvoreBMais;
 import pucflix.aeds3.ParIdId;
+import pucflix.entity.Actor;
 import java.util.ArrayList;
 
-public class ActorFile extends File 
-{
-    ArvoreBMais<ActorNameIdPair> nameIndex;
-    ArvoreBMais<Pair
-    
-    public ActorFile() throws Exception
-    {
-        super("actor", Actor.class.getConstructor());
-        nameIndex = new ArvoreBMais<>(
-            ActorNameIdPair.class.getConstructor(),
-            4,
-            "./dados/" + nomeEntidade + "/nameIndex.db"
-        );
-    }
-	
-    @Override
-    public int create(Actor actor) throws Exception 
-    {
-        int id = super.create(actor);
-        nameIndex.create(new ActorNameIdPair(episode.getName(), id));
-        showRelIndex.create(new ParIdId(actor.getShow(), id));
-        return id;
+public class ActorFile extends Arquivo<Actor> {
+
+  Arquivo<Actor> arqAtores;
+
+  ArvoreBMais<NameIdPair> nameIndex;
+
+  ArvoreBMais<ParIdId> indiceRelacaoSerieAtor;
+
+  public ActorFile() throws Exception {
+
+    super("episodes", Actor.class.getConstructor());
+
+    File directory = new File("./dados/ator");
+    if (!directory.exists()) {
+        directory.mkdirs(); 
     }
 
-    @Override
-    public boolean update(Actor actor) throws Exception 
-    {
-        Actor a = read(actor.getID());
+    nameIndex = new BPlusTree<>(
+      NameIdPair.class.getConstructor(), 5, "./dados/ator" + "indiceNome.db");
 
-        if(a == null) return false;
-        if(!super.update(actor)) return false; 
-        
-        if(!a.getName().equals(actor.getName()))
-        {
-            nameIndex.delete(new ActorNameIdPair(a.getName(), a.getID()));
-            nameIndex.create(new ActorNameIdPair(actor.getName(), actor.getID()));
+    actorSeriesRelationIndex = new BPlusTree<>(
+      IdIdPair.class.getConstructor(), 5, "./dados/ator" + "/indiceRelacaoSerieAtor.db");
+  }
+
+  @Override
+  public int create(Actor actor) throws Exception {
+
+    int id = super.create(actor);
+
+    nameIndex.create(new NameIdPair(actor.getActorName(), id));
+
+    actorSeriesRelationIndex.create(new IdIdPair(actor.getSeriesID(), id));
+
+    return id;
+  }
+
+  public Actor[] readByName(String name) throws Exception {
+
+    if(name.length() == 0) return null;
+
+    ArrayList<NameIdPair> pairs = nameIndex.read(new NameIdPair(name, -1));
+
+    if(!pairs.isEmpty()){
+
+      Actor[] actors = new Actor[pairs.size()];
+
+      int i = 0;
+
+      for(NameIdPair pair : pairs){
+        actors[i++] = read(pair.getId());
+      }
+
+      return actors;
+
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public boolean delete(int id) throws Exception {
+
+    Actor actor = read(id);
+
+    if (actor != null){
+
+      if(super.delete(id)){
+
+        return nameIndex.delete(new NameIdPair(actor.getActorName(), id)) &&
+               actorSeriesRelationIndex.delete(new IdIdPair(actor.getSeriesID(), id));
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean update(Actor updatedActor) throws Exception {
+
+    Actor currentActor = read(updatedActor.getID());
+
+    if(currentActor != null){
+
+      if(super.update(updatedActor)){
+
+        if(!currentActor.getActorName().equals(updatedActor.getActorName())){
+
+          nameIndex.delete(new NameIdPair(currentActor.getActorName(), currentActor.getID()));
+          nameIndex.create(new NameIdPair(updatedActor.getActorName(), updatedActor.getID()));
+
         }
-
         return true;
+      }
+    }
+    return false;
+  }
+
+  public Actor[] readBySeries(int seriesId) throws Exception {
+
+    ArrayList<IdIdPair> pairs = actorSeriesRelationIndex.read(new IdIdPair(seriesId, -1));
+
+    if (pairs.isEmpty()) {
+      return null;
     }
 
-    public Actor[] read(String name) throws Exception
-    {
-        if(name.isEmpty()) return null;
+    Actor[] actors = new Actor[pairs.size()];
+    int i = 0;
 
-        ArrayList<ActorNameIdPair> pairs = nameIndex.read(new ActorNameIdPair(name, -1));
-        if(pairs.size() == 0) return null;
-
-        Actor[] actors = new Actor[pairs.size()];
-        
-        for(int i = 0; i < pairs.size(); i++)
-        {
-            actors[i] = read(pairs.get(i).getID());
-        }
-
-        return actors;
+    for (IdIdPair pair : pairs) {
+      actors[i++] = read(pair.getId2());
     }
 
-    @Override
-    public boolean delete(int id) throws Exception 
-    {
-        Actor actor = read(id);
-        
-        if(actor == null) return false;
-        if(!super.delete(id)) return false;
-        return nameIndex.delete(new ActorNameIdPair(actor.getName(), id));
-    }
-
-    public boolean delete(String name) throws Exception
-    {
-        if(name.isEmpty())
-            return false;
-
-        ArrayList<ActorNameIdPair> pairs = nameIndex.read(new ActorNameIdPair(name, -1));
-        for(ActorNameIdPair pair : pairs)
-        {
-            if(pair.getName().equals(name))
-                return delete(pair.getID());
-        }
-
-        return false;
-    }
+    return actors;
+  }
 }
